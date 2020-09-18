2ff490f38410bfab125fd1224ae362d09a8a0563
SolutionDev888 <developerteam888@outlook.com>
2020-09-18 10:21:47 +0800

    init

---
+++ data.js
@@ -0,0 +1,68 @@
+let data = '{ \
+"data":[ \
+	{"title":"Sleep App", \
+	"comment":"Sleep app is android app.", \
+	"responsible":"Front-end, partial back-end", \
+	"skills":"Android, Kotlin, Firebase, RealmDB, Retrofit", \
+	"imgs":[ "img/works/sleep/1.jpg", \
+			"img/works/sleep/2.jpg", \
+			"img/works/sleep/3.jpg"] \
+	}, \
+	{"title":"Dream App", \
+	"comment":"Dream App is android app.", \
+	"responsible":"Front-end, partial back-end", \
+	"skills":"Android, Kotlin, Firebase, RealmDB, Retrofit", \
+	"imgs":[ "img/works/dreame/1.jpg", \
+			"img/works/dreame/2.jpg", \
+			"img/works/dreame/1.jpg"] \
+	}, \
+	{"title":"JW-CAD View App", \
+	"comment":"JW-CAD View is mobile app for view of JW-CAD files.", \
+	"responsible":"Front-end, partial back-end", \
+	"skills":"Android, Kotlin, OpenCV, Firebase, RealmDB, Retrofit", \
+	"imgs":[ "img/works/jwcad/1.jpg", \
+			"img/works/jwcad/2.jpg", \
+			"img/works/jwcad/3.jpg"] \
+	}, \
+	{"title":"Shooting Game App", \
+	"comment":"This is FPS First person offline shooting game", \
+	"responsible":"Front-end, partial back-end", \
+	"skills":"Unity, Android, Java, RealmDB ", \
+	"imgs":[ "img/works/shooting/1.jpg", \
+			"img/works/shooting/2.jpg", \
+			"img/works/shooting/3.jpg", \
+			"img/works/shooting/4.jpg", \
+			"img/works/shooting/5.jpg"] \
+	}, \
+	{"title":"Loans App", \
+	"comment":"online free service for loans", \
+	"responsible":"Front-end, partial back-end", \
+	"skills":"iOS, Swift, UIKit, RealmDB, Alamofire, Angular", \
+	"imgs":["img/works/loans/2.jpg"] \
+	}, \
+	{"title":"Soccer App", \
+	"comment":"online free service for loans", \
+	"responsible":"Front-end, partial back-end", \
+	"skills":"iOS, Swift, UIKit, GameKit, RealmDB, Alamofire, Angular", \
+	"imgs":[ "img/works/soccer/1.jpg", \
+			"img/works/soccer/2.jpg"] \
+	}, \
+	{"title":"Hand Crowd", \
+	"comment":"A service that helps project managers. Project managers can create projects, split it in tasks or missions, and assign them to employees. The system shows status of the project	visually and helps the manager to find proper employees for a specific task based on their skills and their idle state.", \
+	"responsible":"Front-end, partial back-end", \
+	"skills":"Angular, Bootstrap, PHP, MySQL, Facebook, Google, XCode, Android SDK", \
+	"imgs":[ "img/works/HandCrowd/1.jpg", \
+			"img/works/HandCrowd/2.jpg", \
+			"img/works/HandCrowd/3.jpg", \
+			"img/works/HandCrowd/4.jpg", \
+			"img/works/HandCrowd/5.jpg"] \
+	}, \
+	{"title":"Price Compare Site", \
+	"comment":"A service that helps project managers. Project managers can create projects, split it in tasks or missions, and assign them to employees. The system shows status of the project	visually and helps the manager to find proper employees for a specific task based on their skills and their idle state.", \
+	"responsible":"Front-end, partial back-end", \
+	"skills":"Angular, Bootstrap, PHP, MySQL, Facebook, Google, XCode, Android SDK", \
+	"imgs":[ "img/works/price_compare/1.jpg", \
+			"img/works/price_compare/2.jpg"] \
+	} \
+	]\
+}';
